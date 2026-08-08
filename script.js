a9293ed12e1199c97041264f343cab89ec3b8943
eepynel <andranathanael36@gmail.com>
2026-08-08 23:15:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('guestForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Mencegah form reload halaman
+
+    // Mengambil nilai dari 5 inputan
+    const nama = document.getElementById('nama').value;
+    const email = document.getElementById('email').value;
+    const instansi = document.getElementById('instansi').value;
+    const kategori = document.getElementById('kategori').value;
+    const pesan = document.getElementById('pesan').value;
+
+    // Menampilkan hasil di bawah tombol form
+    document.getElementById('resNama').innerText = "Nama: " + nama;
+    document.getElementById('resEmail').innerText = "Email: " + email;
+    document.getElementById('resInstansi').innerText = "Instansi: " + instansi;
+    document.getElementById('resKategori').innerText = "Kategori: " + kategori;
+    document.getElementById('resPesan').innerText = "Pesan: " + pesan;
+
+    // Tampilkan box hasil
+    document.getElementById('formResult').style.display = "block";
+
+    // Opsi alert (Sesuai instruksi tugas)
+    alert("Terima kasih! Data buku tamu berhasil dikirim.");
+});
